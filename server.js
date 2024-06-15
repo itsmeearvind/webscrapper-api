115eafefb1fd7dc6baddcabde5214aabd86fecaf
Arvind <thearvindd@outlook.com>
2024-06-15 13:03:44 +0530

    homepage mode

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const scrapeRoute = require('./routes/scrape');
 require('dotenv').config();
 
 const app = express();
-
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 const PORT = process.env.PORT || 5001;
 const MONGODB_URI = process.env.MONGO_URI;
 
@@ -17,6 +21,10 @@ mongoose.connect(MONGODB_URI)
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', scrapeRoute);
+
+app.get("/", async (req, res) => {
+  res.status(200).send("<h1 style='text-align: center; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);'>Server is running</h1>");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
